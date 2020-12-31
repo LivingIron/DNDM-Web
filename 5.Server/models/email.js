866ccd637237a67b9fec2983de45224b1eadc583
LivingIron <cveganton@gmail.com>
2020-12-31 16:28:58 +0200

    added prerequsites for emails
    added another schema for emails so i can use it in the future

---
+++ 5.Server/models/email.js
@@ -0,0 +1,23 @@
+const mongoose=require('mongoose');
+const Schema=mongoose.Schema;
+
+const emailSchema=new Schema({
+    email:{
+        type:String,
+        required:true
+    },
+    subject:{
+        type:String,
+        required:true
+    },
+    message:{
+        type:String,
+        required:true
+    }
+
+
+},{timestamps:true});
+
+const Email = mongoose.model('Email',emailSchema);
+
+module.exports=Email;
